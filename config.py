4909efc5e3e866e017475b09ba985c2c3d63f860
Dawid <dawid87872@gmail.com>
2024-05-26 13:55:39 +0200

    Update config
    Change path to app data, Add main executable name

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ from os import getenv
 
 
 class Config:
+    MAIN_EXECUTABLE_NAME = "Wuthering Waves RPC.exe"
     APPLICATION_ID = "1243855663210303488"
     WUWA_PROCESS_NAME = "Wuthering Waves.exe"
     LOCAL_DATABASE_PATH = "C:\\Wuthering Waves\\Wuthering Waves Game\\Client\\Saved\\LocalStorage\\LocalStorage.db"
-    LOCAL_APP_DATA_PATH = join(getenv("LOCALAPPDATA"), "WutheringWavesRPC")
     """
     This is the default path to the database. I would like to make this configurable somehow
     """
+    LOCAL_APP_DATA_PATH = join(getenv("LOCALAPPDATA"), "Wuthering Waves RPC")
